aaa88a8d87134525135ae7fcff2a35378f642cff
Andreick <andreick.gomes.ismart@gmail.com>
2022-08-21 00:55:12 -0300

    Add WiFi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,71 @@
-#include <Wire.h>
-#include "MAX30105.h"
-#include "spo2_algorithm.h"
+#include "SPIFFS.h"
+#include "WiFi.h"
 
-MAX30105 particleSensor;
+#define WIFI_TIMEOUT 20000
 
-uint32_t irBuffer[100];  // infrared LED sensor data
-uint32_t redBuffer[100]; // red LED sensor data
-int32_t spo2;            // SPO2 value
-int8_t validSPO2;        // indicator to show if the SPO2 calculation is valid
-int32_t heartRate;       // heart rate value
-int8_t validHeartRate;   // indicator to show if the heart rate calculation is valid
+const char *ssidPath = "/wifi-ssid.txt";
+const char *passPath = "/wifi-pass.txt";
+
+void initSPIFFS()
+{
+    if (!SPIFFS.begin())
+    {
+        ESP.restart();
+    }
+}
+
+String readFile(fs::FS &fs, const char *path)
+{
+    Serial.printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if (!file || file.isDirectory())
+    {
+        Serial.println("- failed to open file for reading");
+        return String();
+    }
+
+    String fileContent;
+    while (file.available())
+    {
+        fileContent = file.readStringUntil('\n');
+        break;
+    }
+    return fileContent;
+}
+
+void connectToWifi()
+{
+    String ssid = readFile(SPIFFS, ssidPath);
+    String pass = readFile(SPIFFS, passPath);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(&ssid[0], &pass[0]);
+
+    Serial.print("Connecting to WiFi");
+    unsigned long startAttemptMs = millis();
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        if (millis() - startAttemptMs > WIFI_TIMEOUT)
+        {
+            Serial.print("\nConnection failed, restarting...");
+            ESP.restart();
+        }
+        Serial.print(".");
+        delay(100);
+    }
+
+    Serial.print("Connected on IP: ");
+    Serial.println(WiFi.localIP());
+}
 
 void setup()
 {
-    Serial.begin(115200); // initialize serial communication at 115200 bits per second:
-
-    // Initialize sensor
-    if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) // Use default I2C port, 400kHz speed
-    {
-        Serial.println(F("MAX30105 was not found. Please check wiring/power."));
-        for (;;)
-            ;
-    }
-
-    Serial.println(F("Attach sensor to finger with rubber band. Press any key to start conversion"));
-    while (Serial.available() == 0)
-        ; // wait until user presses a key
-    Serial.read();
-
-    byte ledBrightness = 60;   // Options: 0=Off to 255=50mA
-    byte sampleAverage = 4;    // Options: 1, 2, 4, 8, 16, 32
-    byte ledMode = 2;          // Options: 2 = Red + IR
-    byte sampleRate = 100;     // Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
-    uint16_t pulseWidth = 411; // Options: 69, 118, 215, 411
-    uint16_t adcRange = 4096;  // Options: 2048, 4096, 8192, 16384
-
-    particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); // Configure sensor with these settings
+    Serial.begin(115200);
+    initSPIFFS();
+    connectToWifi();
 }
 
 void loop()
 {
-    for (byte i = 0; i < 100; i++) // read the first 100 samples, and determine the signal range
-    {
-        while (particleSensor.available() == false) // do we have new data?
-            particleSensor.check();                 // Check the sensor for new data
-
-        redBuffer[i] = particleSensor.getRed();
-        irBuffer[i] = particleSensor.getIR();
-        particleSensor.nextSample(); // We're finished with this sample so move to next sample
-
-        Serial.print(F("red="));
-        Serial.print(redBuffer[i], DEC);
-        Serial.print(F(", ir="));
-        Serial.println(irBuffer[i], DEC);
-    }
-
-    // calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
-    maxim_heart_rate_and_oxygen_saturation(irBuffer, 100, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
-
-    for (;;) // Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second
-    {
-        Serial.print(F("HR="));
-        Serial.print(heartRate, DEC);
-        Serial.print(F(", HRvalid="));
-        Serial.print(validHeartRate, DEC);
-        Serial.print(F(", SPO2="));
-        Serial.print(spo2, DEC);
-        Serial.print(F(", SPO2Valid="));
-        Serial.println(validSPO2, DEC);
-
-        for (byte i = 25; i < 100; i++) // dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
-        {
-            redBuffer[i - 25] = redBuffer[i];
-            irBuffer[i - 25] = irBuffer[i];
-        }
-
-        for (byte i = 75; i < 100; i++) // take 25 sets of samples before calculating the heart rate.
-        {
-            while (particleSensor.available() == false) // do we have new data?
-                particleSensor.check();                 // Check the sensor for new data
-
-            redBuffer[i] = particleSensor.getRed();
-            irBuffer[i] = particleSensor.getIR();
-            particleSensor.nextSample(); // We're finished with this sample so move to next sample
-        }
-
-        // After gathering 25 new samples recalculate HR and SP02
-        maxim_heart_rate_and_oxygen_saturation(irBuffer, 100, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
-    }
 }
